e829942a0a61795497aa522c4cf505f5596a85f4
Joshua Jarvis <joshuajarvis0711@gmail.com>
2020-01-22 16:17:00 -0700

    Remove prod stuff from master

--- src/resources/const.py
+++ src/resources/const.py
@@ -3,6 +3,3 @@ BLACK = 1
 WHITE = 2
 # WEBSITE_PATH = '/home/ixonstater/code/go_client/'
 # WEBSITE_PATH = '/Users/otheruser/bin/lib/go-client'
-
-#production path
-WEBSITE_PATH = '/home/ixonstater/go/client'
