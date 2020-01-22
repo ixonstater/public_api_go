a817e42a01ee8a63058e8ab4922b23310ffdadb2
Joshua Jarvis <joshuajarvis0711@gmail.com>
2020-01-22 16:12:21 -0700

    Added production path to client

--- src/resources/const.py
+++ src/resources/const.py
@@ -2,4 +2,7 @@ EMPTY = 0
 BLACK = 1
 WHITE = 2
 # WEBSITE_PATH = '/home/ixonstater/code/go_client/'
-WEBSITE_PATH = '/Users/otheruser/bin/lib/go-client'
+# WEBSITE_PATH = '/Users/otheruser/bin/lib/go-client'
+
+#production path
+WEBSITE_PATH = '/home/ixonstater/go/client'
