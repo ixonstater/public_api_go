700c7a8b7cebcb712060303818a90a48bdbb2fa0
Joshua Jarvis <joshuajarvis0711@gmail.com>
2020-01-25 14:58:41 -0700

    Changed database commit schedule

--- src/resources/const.py
+++ src/resources/const.py
@@ -6,7 +6,7 @@ DELETE_MATCH_TIMEOUT = 3600
 BG_SLEEP_INTERVAL = 5
 RESET_COUNT = 1000
 REMOVE_MATCHES_COUNT = 1000
-MOVE_TO_DB_COUNT = 1
+MOVE_TO_DB_COUNT = 100
 
 WEBSITE_PATH = '/home/ixonstater/code/go_client/'
 # WEBSITE_PATH = '/Users/otheruser/bin/lib/go-client'
