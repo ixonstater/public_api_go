380c51944bf7d7c161337144d64f54c1f3e4a7e8
Joshua Jarvis <joshuajarvis0711@gmail.com>
2020-02-23 08:11:05 -0700

    New script for sending builds to server, wsgi connector uses relative path now

--- src/wsgi_connector.py
+++ src/wsgi_connector.py
@@ -1,4 +1,4 @@
 from pyramid.paster import get_app, setup_logging
 
-ini_path = '/home/ixonstater/code/public_api_go/src/production.ini'
+ini_path = 'src/production.ini'
 application = get_app(ini_path, 'main')
